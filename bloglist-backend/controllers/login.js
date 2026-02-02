6a731ac66451b4b35106900344ff56de93f8dc69
jessiekeng <zixingkeng@gmail.com>
2026-02-02 11:06:42 +0800

    Update loginRouter.post

--- bloglist-backend/controllers/login.js
+++ bloglist-backend/controllers/login.js
@@ -25,8 +25,8 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(userForToken, process.env.SECRET)
 
   response
-    .status(200)
-    .send({ token, username: user.username, name: user.name })
+  .status(200)
+  .send({ token, username: user.username, name: user.name, role: user.role })
 })
 
 module.exports = loginRouter
